40d04f68c00aa295ee1b75caa8d5503d2de102f6
Ross Keeley <rjkeeley@gmail.com>
2020-12-29 23:41:11 +0000

    Added buttons with javascript

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 
+var skills = ["HTML", "CSS", "JavaScript"];
+
+// var btn = document.createElement("button");
+// document.getElementById('skills-card').appendChild(btn).innerHTML = "HI";
+
+skills.forEach((i) => {
+  var btn = document.createElement("button");
+
+  document.getElementById('skills-card').appendChild(btn).innerHTML = i;
+});
